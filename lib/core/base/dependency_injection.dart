1449f76f62bbac0b882beef62f7c730fd7b882f9
mano270699 <mymanos2000@gmail.com>
2025-08-20 23:35:28 +0300

    config ios

--- lib/core/base/dependency_injection.dart
+++ lib/core/base/dependency_injection.dart
@@ -1,7 +1,6 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:path_provider/path_provider.dart';
 import '../../src/layout/screens/main_screen_view_model.dart';
 import '../../src/splash_screen/view/splash_screen_view_model.dart';
@@ -10,7 +9,6 @@ import '../util/api_interceptor/api_interceptor.dart';
 import '../util/localization/cubit/localization_cubit.dart';
 import '../util/localization/localization_cache_helper.dart';
 import '../util/localization/models/localization_model.dart';
-import '../util/network/network_info.dart';
 import '../util/network_service.dart';
 import '../util/routes/routes_reader.dart';
 import 'route_genrator.dart';
@@ -24,13 +22,11 @@ Future<void> init() async {
 
   sl.registerFactory(() => RouteGenerator(routs: sl()));
 
-  sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
+  // sl.registerLazySingleton<NetworkInfo>(() => NetworkInfoImpl(sl()));
 
   sl.registerFactory(() => LocalizationCasheHelper());
 
   sl.registerFactory(() => LocalizationCubit(sl()));
-
-  sl.registerLazySingleton(() => InternetConnectionChecker());
 
   sl.registerLazySingleton<NetworkService>(() => NetworkServiceImpl());
 
